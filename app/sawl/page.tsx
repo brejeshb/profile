6f12f886cccd45751d7bec92aaa3601ac74c85bc
Brejeshb <brejeshb.2023@scis.smu.edu.sg>
2026-03-01 17:36:54 +0800

    added coursework sub page

---
+++ app/sawl/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const sawl = () => {
+  return (
+    <div>
+        <img src="/images/1.png">
+        </img>
+        <img src="/images/2.png">
+        </img>
+    </div>
+  )
+}
+
+export default sawl
